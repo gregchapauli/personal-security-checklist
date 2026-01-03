e94d1286c382e575986ec79ffb6a98f131ef1e48
Grégory C. <60263116+gregchapauli@users.noreply.github.com>
2026-01-03 22:04:22 +0100

    Update hero.tsx

--- web/src/components/furniture/hero.tsx
+++ web/src/components/furniture/hero.tsx
@@ -7,7 +7,7 @@ export default component$(() => {
     <div class="hero mb-8 mx-auto xl:max-w-7xl max-w-6xl w-full xl:px-10">
       <div class="hero-content text-center bg-front shadow-sm lg:rounded-xl w-full">
         <div class="max-w-2xl flex flex-col place-items-center">
-          //<p>The Ultimate</p>
+          <p>The Ultimate</p>
           <h1 class="text-5xl font-bold">Personal Security Checklist</h1>
           <p class="subtitle pb-6">Your guide to securing your digital life and protecting your privacy</p>
           <Icon class="mb-4" icon="shield" width={120} height={120}  />
