bd05f612e70c6ec348cb7fc6994e1145dfdb1d39
Grégory C. <60263116+gregchapauli@users.noreply.github.com>
2026-01-03 22:43:27 +0100

    Update nav.tsx

--- web/src/components/furniture/nav.tsx
+++ web/src/components/furniture/nav.tsx
@@ -32,7 +32,7 @@ export default component$(() => {
     <>
       <input id="my-drawer-3" type="checkbox" class="drawer-toggle" /> 
       <div class="navbar bg-base-100">
-        {/*<div class="flex-1">
+        <div class="flex-1">
           <div class="flex-none md:hidden">
             <label for="my-drawer-3" aria-label="open sidebar" class="btn btn-square btn-ghost">
               <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="inline-block w-6 h-6 stroke-current"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16"></path></svg>
@@ -42,7 +42,7 @@ export default component$(() => {
             <label for="my-drawer-3" aria-label="open sidebar" class="tooltip tooltip-bottom" data-tip="View all Pages"><Icon class="mr-2" icon="shield" width={28} height={28}  /></label>
             <h1>Digital Defense</h1>
           </a>
-        </div>*/}
+        </div>
         <div class="flex-none hidden md:flex">
           <ul class="menu menu-horizontal px-1">
             <li>
